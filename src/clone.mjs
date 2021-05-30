b1a5765c5fb28f3720dfc1666299a59c0dcdd6e9
Alan Layng <alan@readersludlow.com>
2021-05-30 14:32:16 +0100

    tidy:use Object.fromEntries

--- src/clone.mjs
+++ src/clone.mjs
@@ -2,8 +2,5 @@ export default function clone (o) {
   if (!o || typeof o !== 'object') return o
   if (o instanceof Date) return new Date(o)
   if (Array.isArray(o)) return o.map(clone)
-  return Object.entries(o).reduce((o, [k, v]) => {
-    o[k] = clone(v)
-    return o
-  }, {})
+  return Object.fromEntries(Object.entries(o).map(([k, v]) => [k, clone(v)]))
 }
